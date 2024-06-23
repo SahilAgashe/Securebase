6282dd2c59e18682d4cb3a94d19208239242dcff
Sahil Agashe <sahilagashe2002@gmail.com>
2024-06-23 18:23:51 +0530

    minor changes

--- Securebase/Application/AppDelegate.swift
+++ Securebase/Application/AppDelegate.swift
@@ -7,13 +7,19 @@
 
 import UIKit
 
+private let kDebugAppDelegate = "DEBUG AppDelegate: "
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print(kDebugAppDelegate, #function)
+        return true
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print(kDebugAppDelegate, #function)
         return true
     }
 
@@ -22,6 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
+        print(kDebugAppDelegate, #function)
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
@@ -29,6 +36,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        print(kDebugAppDelegate, #function)
     }
 
 
